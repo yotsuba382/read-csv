aefd6526f4c21ed3f94807f3f3b2b22986a3e43a
RyotaFukuzawa <s1220241@u-aizu.ac.jp>
2017-11-01 01:13:37 +0900

    fix

--- ReadCSV.java
+++ ReadCSV.java
@@ -12,24 +12,31 @@ public class ReadCSV {
     String[] string_csv = {"person1_a.csv","person1_i.csv","person1_u.csv",
                             "person2_a.csv","person2_i.csv","person2_u.csv"};
 
+    int label = 0;
     for(int i=0; i<6 ; i++){
       try {
 
-        System.out.print("hello");
         File csv = new File(string_csv[i]); // CSVデータファイル
+        
+        if(i==3) label = 1; 
  
         BufferedReader br = new BufferedReader(new FileReader(csv));
  
         // 最終行まで読み込む
         String line = "";
+        int count = 1;
+
         while ((line = br.readLine()) != null) {
  
+        count = 1; 
           // 1行をデータの要素に分割
           StringTokenizer st = new StringTokenizer(line, ",");
  
+          System.out.print(label+" "); //label
           while (st.hasMoreTokens()) {
             // 1行の各要素をスペース" "区切りで表示
-            System.out.print(st.nextToken() + " ");
+            System.out.print(count+":"+st.nextToken()+" ");
+            count++;
           }
           System.out.println();
         }
